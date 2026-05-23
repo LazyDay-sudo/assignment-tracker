0858ddfdef85449dc62a8144c58b41c10296e111
LazyDay-sudo <austinb.mead@gmail.com>
2026-05-22 20:18:04 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/route', (req, res, next) => {
+app.use('/api', (req, res, next) => {
+    // In Vercel, the path comes after /route/
     const targetUrl = req.url.substring(1); 
     if (!targetUrl) return res.status(400).send('No target URL provided.');
 
@@ -23,6 +20,4 @@ app.use('/route', (req, res, next) => {
     })(req, res, next);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server active on port ${PORT}`);
-});
+module.exports = app;
